d4bcbae1a3470eb87651c60ed7979579ec23dea6
johan <stjernholm@msn.com>
2019-05-22 15:17:06 +0800

    Feat: failing to sort out new item and category logic

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -25,7 +25,7 @@ def catalog():
     return render_template('catalog.html', categories = categories, items = items, list_categories = list_categories)
 
 
-@app.route('/catalog/new', methods = ['GET', 'POST'])
+@app.route('/catalog/category/new', methods = ['GET', 'POST'])
 def addCategory():
     if request.method == 'POST':
         if request.form['category_name']:
@@ -49,25 +49,44 @@ def showCategory(category_name):
     return render_template('showCategory.html', category = category, categories = categories, items = items)
 
 
-@app.route('/catalog/<category_name>/new', methods = ['GET', 'POST'])
-def addItem(category_name):
-    category = session.query(Category).filter_by(category_name = category_name).one()
-    categories = session.query(Category).order_by(Category.category_name)
-    if request.method == 'POST':
+@app.route('/catalog/item/new', methods = ['GET', 'POST'])
+def addItem():
+    categories = session.query(Category).order_by(Category.category_name).all()
+    if not categories and request.method == 'POST':
+        if request.form['item_name'] and request.form['category_name']:
+            newCategory = Category(category_name = request.form['category_name'])
+            session.add(newCategory)
+            session.commit()
+            addedCategory = session.query(Category).first()
+            newItem = Item(item_name = request.form['item_name'],
+                           item_description = request.form['item_description'],
+                           item_price = request.form['item_price'],
+                           category_id = addedCategory.category_id)
+            session.add(newItem)
+            session.commit()
+            flash('New Item added!')
+            return redirect(url_for('catalog'))
+        else:
+            flash('You did not add a new Item')
+            return redirect(url_for('catalog'))
+
+    elif request.method == 'POST':
         if request.form['item_name']:
             newItem = Item(item_name = request.form['item_name'],
                            item_description = request.form['item_description'],
                            item_price = request.form['item_price'],
-                           category_id = category.category_id)
+                           category_id = request.form['category_id'])
             session.add(newItem)
             session.commit()
             flash('New Item added!')
-            return redirect(url_for('showCategory', category_name = category.category_name))
+            return redirect(url_for('catalog'))
         else:
             flash('You did not add a new Item')
-            return redirect(url_for('showCategory', category_name = category.category_name))
+            return redirect(url_for('catalog'))
+    elif not categories and request.method == 'GET':
+        return render_template('addCategoryItem.html')
     else:
-        return render_template('addItem.html', category = category, categories = categories)
+        return render_template('addItem.html', categories = categories)
 
 
 @app.route('/catalog/<category_name>/<item_name>')
